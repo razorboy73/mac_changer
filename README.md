4760489905de88b67258d7e01103fa0a6b7ab42c
root <root@kali.kali>
2024-12-21 11:06:03 -0600

    refactored code base and added specifications in readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # mac_changer
+
+
+uses subprocess library
+
+Need input fields for interfaces
+Need a script to identify network interfaces and output them to the main view.
+
+need a script to generate fake new mac addresses.  Provide user a button that says Generate New Mac Address.  Store previously used fake mac addresses, with time and date of its use in database.  Fake mac addresses should always be unique and never recycled.   Mac address are in the format of xx:xx:xx:xx:xx:xx
+
+Provide the user with a drop down menu with a list of the network interfaces that are avialble to change
+
+Provide user with a button that the user can click to change the interface
+
+
+
+Store orignally used mac address so that it can be reverted to 
+
+
+
+alert user that the mac address has been changed
+
+allow user to revert to original mac address by pushing a button that says revert back to original mac address
+Alert user that he has revert back to original mac address
+Display any error messages to the user
--- mac_changer.py
+++ mac_changer.py
@@ -8,78 +8,20 @@ original_mac = ""
 changed_mac = ""
 
 """
-this program changes the MAC address for the indicated interface
-1. Get input from user for interface and MAC address via command line
-2. checks if the interface has been changed
-3. changes interface address
-
-
+This program changes the MAC address for the specified interface.
+1. Get input from the user for interface and MAC address via command line.
+2. Validates the MAC address format.
+3. Checks and changes the MAC address.
+4. Confirms whether the MAC address was successfully updated.
 """
 
 
-def main():
-    global original_mac
-    global changed_mac
-    #get command line arguments
-    interface, new_mac = get_arguments()
-    #check_interface uses find_mac to find mac in the if config results
-    ifconfig_result = check_interface(interface)
-    print(f'Current MAC Address: {ifconfig_result}')
-    #set original mac address
-    original_mac = ifconfig_result
-    #call the chnge mac function
-    change_mac(interface, new_mac)
-    #check that it swapped uses find_mac to find mac in the if config results
-    ifconfig_result = check_interface(interface)
-    #print(ifconfig_result)
-    changed_mac = ifconfig_result
-    print(confirm_changes(new_mac))
-
-
-def confirm_changes(new_mac):
-    global original_mac
-    global changed_mac
-    # if original_mac != changed_mac:
-    #     return("MAC Address successfully changed")
-    #     else:
-    #   return("[-]MAC Address not changed")
-    if changed_mac == new_mac:
-        return("Confirming that MAC Address successfully changed to the requested MAC")
-    else:
-        return("[-]Confirming MAC Address not changed")
-
-
-
-def find_mac(ifconfig_results):
-    # Using re.search to find the MAC address
-    pattern = rb'ether ([0-9a-fA-F:]{17})'
-    mac_address_search_result = re.search(pattern, ifconfig_results)
-    #print(f"mac_address_search_result: {mac_address_search_result}")
-    if mac_address_search_result:
-        mac_address = mac_address_search_result.group(1).decode()
-        return mac_address
-    else:
-        return ('[-]No MAC address found')
-
-
-def check_interface(interface):
-    #get result from ifconfig
-    ifconfig_results = subprocess.check_output(["ifconfig", interface])
-    mac_address = find_mac(ifconfig_results)
-    return mac_address
-
-
-def change_mac(interface, new_mac):
-    print(f"Changing MAC address for {interface} to {new_mac}")
-    #subprocess.call waits for command to execute
-    #use a list of parameters to prevent malicious commands
-    subprocess.call(["ifconfig", f"{interface}", "down"])
-    subprocess.call(["ifconfig", f"{interface}", "hw", "ether", f"{new_mac}"])
-    subprocess.call(["ifconfig", f"{interface}", "up"])
-    # print(f"MAC address for {interface} has been changed to {new_mac}")
 
 
 def get_arguments():
+    """
+    Parses command line arguments for interface and MAC address.
+    """
     #this creates a parser object
     parser = argparse.ArgumentParser(description='Input the interface and MAC address')
     #adds the two agurments we want to collect
@@ -95,6 +37,95 @@ def get_arguments():
     # handled errors for the mac address within the arguments
     return args.interface, args.new_mac
 
+
+def validate_mac(mac):
+    """Validates mack address format"""
+    pattern = r"^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$"
+    return re.match(pattern, mac) is not None
+
+def find_mac(ifconfig_results):
+    """
+    Finds and returns the MAC address from the ifconfig output.
+    """
+    pattern = rb'ether ([0-9a-fA-F:]{17})'
+    mac_address_search_result = re.search(pattern, ifconfig_results)
+    #print(f"mac_address_search_result: {mac_address_search_result}")
+    if mac_address_search_result:
+        mac_address = mac_address_search_result.group(1).decode()
+        return mac_address
+    else:
+        return None
+
+def check_interface(interface):
+    """
+    Retrieves the current MAC address for the specified interface.
+    """
+    try: 
+        ifconfig_results = subprocess.check_output(["ifconfig", interface],stderr=subprocess.STDOUT)
+        return find_mac(ifconfig_results)
+    
+    except subprocess.CalledProcessError as e:
+        print(f"[-] Error checking interface: {e.output.decode().strip()}")
+        return None
+    
+
+def change_mac(interface, new_mac):
+    """
+    Changes the MAC address for the specified interface.
+    """
+    print(f"Changing MAC address for {interface} to {new_mac}")
+    #subprocess.call waits for command to execute
+    #use a list of parameters to prevent malicious commands
+    try:
+        subprocess.run(["ifconfig", f"{interface}", "down"])
+        subprocess.run(["ifconfig", f"{interface}", "hw", "ether", f"{new_mac}"])
+        subprocess.run(["ifconfig", f"{interface}", "up"])
+        print(f"MAC address for {interface} has been changed to {new_mac}")
+    except subprocess.CalledProcessError as e:
+        print(f"[-] Error changing MAC address: {e}")
+
+def confirm_changes(new_mac):
+    """
+    Confirms if the MAC address has been successfully updated.
+    """
+    # if original_mac != changed_mac:
+    #     return("MAC Address successfully changed")
+    #     else:
+    #   return("[-]MAC Address not changed")
+    if changed_mac == new_mac:
+        return True
+    else:
+        return False
+
+
+def main():
+    
+    #get command line arguments
+    interface, new_mac = get_arguments()
+    #check_interface uses find_mac to find mac in the if config results
+
+    #validate the MAC address format
+    if not validate_mac(new_mac):
+        print("[-] Invalid MAC address format. Please enter a valid MAC address.")
+        return
+    
+
+    #check and display the original MAC address
+    original_mac = check_interface(interface)
+    if not original_mac:
+        print("[-] Unable to retrieve the current MAC address.")
+        return
+    print(f"Current MAC Address: {original_mac}")
+
+    # Verify if the MAC address was updated
+    updated_mac= change_mac(interface, new_mac)
+    #check that it swapped uses find_mac to find mac in the if config results
+
+    if updated_mac:
+          return f"[+] MAC Address successfully changed to {new_mac}"
+    else:
+        return "[-] MAC Address change failed."
+    print(confirm_changes(new_mac))
 
 if __name__ == "__main__":
     main()
